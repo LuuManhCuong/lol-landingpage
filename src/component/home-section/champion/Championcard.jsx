cb61fc9d30dc69754f86f87af128f9496796fc1a
cuong <cuong1042k1@gmail.com>
2022-10-13 20:17:22 +0700

    deloy

--- src/component/home-section/champion/Championcard.jsx
+++ src/component/home-section/champion/Championcard.jsx
@@ -1,51 +1,55 @@
-import { useRef } from 'react'
-import './champion-card.scss'
+import { useRef } from "react";
+import "./champion-card.scss";
 
-const ChampionCard = props => {
+const ChampionCard = (props) => {
+  const cardRef = useRef(null);
 
-    const cardRef = useRef(null)
+  const item = props.item;
 
-    const item = props.item
+  const onClick = () => {
+    const img = cardRef.current.querySelector("img");
+    const pos = img.getBoundingClientRect();
 
-    const onClick = () => {
-        const img = cardRef.current.querySelector('img')
-        const pos = img.getBoundingClientRect()
+    const newNode = img.cloneNode(true);
+    newNode.style.width = img.offsetWidth + "px";
+    newNode.style.height = img.offsetHeight + "px";
+    newNode.style.position = "absolute";
+    newNode.style.top = pos.top + "px";
+    newNode.style.left = pos.left + "px";
+    newNode.style.zIndex = "102";
 
-        const newNode = img.cloneNode(true)
-        newNode.style.width = img.offsetWidth + 'px'
-        newNode.style.height = img.offsetHeight + 'px'
-        newNode.style.position = 'absolute'
-        newNode.style.top = pos.top + 'px'
-        newNode.style.left = pos.left + 'px'
-        newNode.style.zIndex = '102'
+    newNode.style.transition = "all 0.7s ease";
+    newNode.id = `champ-img-${props.id}`;
 
-        newNode.style.transition = 'all 0.7s ease'
-        newNode.id = `champ-img-${props.id}`
+    setTimeout(() => {
+      newNode.style.width = img.offsetWidth + "px";
+      // newNode.style.height = '100%'
+      newNode.style.top = "100px";
+      newNode.style.left = "50px";
+    });
 
-        setTimeout(() => {
-            newNode.style.width = img.offsetWidth + 'px'
-            // newNode.style.height = '100%'
-            newNode.style.top = '100px'
-            newNode.style.left = '50px'
-        },)
+    document.body.appendChild(newNode);
 
-        document.body.appendChild(newNode)
+    const videoUrl = `https://youtube.com/embed/${item.video}`;
 
-        const videoUrl = `https://youtube.com/embed/${item.video}`
-        
-        document.querySelector(`#champ-detail-${props.id} iframe`).setAttribute('src', videoUrl)
-        document.querySelector(`#champ-detail-${props.id}`).classList.add('active')
-    }
+    document
+      .querySelector(`#champ-detail-${props.id} iframe`)
+      .setAttribute("src", videoUrl);
+    document.querySelector(`#champ-detail-${props.id}`).classList.add("active");
+  };
 
-    return (
-        <div className="champion-card" onClick={onClick} ref={cardRef}>
-            <div className="frame">
-                <div className="bg-image overlay bg" style={{backgroundImage: `url(${item.bg})`}}></div>
-            </div>
-            <img src={item.img} alt="" />
-            <div className="name">{item.name}</div>
-        </div>
-    )
-}
+  return (
+    <div className="champion-card" onClick={onClick} ref={cardRef}>
+      <div className="frame">
+        <div
+          className="bg-image overlay bg"
+          style={{ backgroundImage: `url(${item.bg})` }}
+        ></div>
+      </div>
+      <img src={item.img} alt="" />
+      <div className="name">{item.name}</div>
+    </div>
+  );
+};
 
-export default ChampionCard
+export default ChampionCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,32 @@
+import { Navigation, Pagination, Mousewheel, EffectFade } from "swiper";
 
-import { Navigation, Pagination, Mousewheel, EffectFade } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-fade'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
 
 const swiperOptions = {
-  direction: 'vertical',
+  direction: "vertical",
   slidesPerView: 1,
   spaceBetween: 0,
   mousewheel: true,
   pagination: true,
-  effect: 'fade',
-  speed: 1000
-}
+  effect: "fade",
+  speed: 1000,
+};
 
-import { Champion, Welcome, ChampionDetail, Trailer, Credit } from "../component/home-section"
+import {
+  Champion,
+  Welcome,
+  ChampionDetail,
+  Trailer,
+  Credit,
+} from "../component/home-section";
 
-import { championsData } from '../assets/dummy';
+import { championsData } from "../assets/dummy";
 
 export default function Home() {
   return (
@@ -31,33 +36,29 @@ export default function Home() {
         modules={[Navigation, Pagination, Mousewheel, EffectFade]}
       >
         <SwiperSlide>
-          {({ isActive }) => <Welcome isActive={isActive}/>}
+          {({ isActive }) => <Welcome isActive={isActive} />}
         </SwiperSlide>
 
         <SwiperSlide>
-          {({ isActive }) => <Champion isActive={isActive}/>}
+          {({ isActive }) => <Champion isActive={isActive} />}
         </SwiperSlide>
 
         <SwiperSlide>
-          {({ isActive }) => <Trailer isActive={isActive}/>}
+          {({ isActive }) => <Trailer isActive={isActive} />}
         </SwiperSlide>
 
         <SwiperSlide>
-          {({ isActive }) => <Credit isActive={isActive}/>}
+          {({ isActive }) => <Credit isActive={isActive} />}
         </SwiperSlide>
       </Swiper>
 
-      {
-        championsData.map((item, index) => <ChampionDetail
-          key={index}
-          item={item}
-          id= {index}
-        ></ChampionDetail>) 
-      }
+      {championsData.map((item, index) => (
+        <ChampionDetail key={index} item={item} id={index}></ChampionDetail>
+      ))}
 
       <div className="scroll">
         <span>Scroll to see effect</span>
       </div>
     </>
   );
-};
+}
